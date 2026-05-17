64ba56dd49f79b204e0e0c447d9db79b86721a6e
atu1ya <24225113@student.uwa.edu.au>
2026-05-17 21:53:58 +0800

    feat: update database configuration to support PostgreSQL

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
 import os
 
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///smartmeet.db'
+
+    _db_url = os.environ.get('DATABASE_URL')
+    if _db_url:
+        if _db_url.startswith('postgres://'):
+            _db_url = _db_url.replace('postgres://', 'postgresql://', 1)
+        SQLALCHEMY_DATABASE_URI = _db_url
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///smartmeet.db'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
 
